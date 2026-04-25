2004535dd586bcf528f57de597a7844ed884bff8
Pranav Vashi <neobuddy89@gmail.com>
2026-04-26 01:47:00 +0530

    ThemeStore: Fix navigation bar overlap
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/axion/axthemestore/ui/ThemeDetailScreen.kt
+++ src/com/android/axion/axthemestore/ui/ThemeDetailScreen.kt
@@ -91,7 +91,11 @@ fun ThemeDetailScreen(
                 tonalElevation = 3.dp,
                 shadowElevation = 1.dp
             ) {
-                Box(modifier = Modifier.padding(16.dp)) {
+                Box(
+                    modifier = Modifier
+                        .navigationBarsPadding()
+                        .padding(16.dp)
+                ) {
                     InstallSection(
                         theme = theme,
                         installState = installState,
