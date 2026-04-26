f2a6483c0eec7481c28e4f7c8eae0439f7660806
Saikrishna1504 <saikrishna26918@gmail.com>
2026-04-26 20:46:15 +0000

    ThemeStore: Fix navigation bar overlap
    Change-Id: Ibe61cf84b99aaf9382580b1a0c814bbb5bcb707c
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/axion/axthemestore/MainActivity.kt
+++ src/com/android/axion/axthemestore/MainActivity.kt
@@ -22,6 +22,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
@@ -30,7 +31,7 @@ import com.android.axion.axthemestore.ui.theme.AxThemeStoreTheme
 import com.android.axion.axthemestore.viewmodel.ThemeStoreViewModel
 
 class MainActivity : ComponentActivity() {
-    
+
     private val viewModel: ThemeStoreViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +41,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AxThemeStoreTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().navigationBarsPadding(),
                     color = MaterialTheme.colorScheme.surfaceBright
                 ) {
                     MainScreen(viewModel = viewModel)
